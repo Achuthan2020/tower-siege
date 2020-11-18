a20cfa494ac71bb876791a5f8e204f5dbec933d2
Achuthan2020 <72617276+Achuthan2020@users.noreply.github.com>
2020-11-18 15:43:50 +0530

    Add files via upload

---
+++ HexagonalBall.js
@@ -0,0 +1,10 @@
+class Hexagonalball {
+    constructor(x,y,width,height){
+        var options={
+            'restitution':0.5,
+            'friction':0.3,
+            'density':1.0
+        }
+    }
+}
+
